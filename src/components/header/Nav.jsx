a54aeaed3b323c2a6260b8184adf97e18fd00b45
protege47007 <davidak.a@yahoo.com>
2022-12-19 18:07:23 +0100

    refactored component functionality

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-function Nav(props) {
+function Nav({linkClassName, navContainer}) {
     const data = [
       <Link to={"/"}>Home</Link>, 
       <Link to={"relationship"}>Relationship Takes</Link>, 
@@ -10,12 +10,12 @@ function Nav(props) {
       <Link to={"contact"}>Contact Me</Link>
     ]
   const listItems = data.map((b, index) => (
-    <li className={`nav-link ${props.linkClassName} text-blue-700 text-xl underline underline-offset-2 py-1`} key={index}>
+    <li className={` ${linkClassName} text-blue-700 text-xl underline underline-offset-2 py-1`} key={index}>
       {b}
     </li>
   ))
 
-  return <ul className={`nav-list ${props.navContainer}`}>{listItems}</ul>
+  return <ul className={`nav-list ${navContainer}`}>{listItems}</ul>
 }
 
 export default Nav
