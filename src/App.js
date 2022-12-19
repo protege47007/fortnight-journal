fa0c68b7eff633265a273f9dfd722f120c8fe03f
protege47007 <davidak.a@yahoo.com>
2022-12-19 15:57:25 +0100

    new refactored pages

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import Login from "./pages/auth/Login"
+// import Reg from "./pages/auth/Reg"
+// import Home from "./pages/Home"
+// import Contact from "./pages/Contact"
+import Compose from "./pages/admin/Compose"
+
+
+
+// import Signup from "./HOC/Signup"
+// import Archive from "./pages/admin/Archive"
+// import Drafts from "./pages/admin/Drafts"
+// import Trash from "./pages/admin/Trash"
+
+
+function App() {
+  return (
+    <div className="font-fontC font-light ">
+      {/* <Login/> */}
+      {/* <Reg/> */}
+      {/* <Home/> */}
+      {/* <Contact/> */}
+      <Compose/>
+
+
+      {/* <Signup/> */}
+      {/* <Trash/> */}
+      {/* <Drafts/> */}
+      {/* <Archive /> */}
+    </div>
+  )
+}
+
+export default App
