ec9c8a9f433afec9ae8be9ec4da0b76b185e38b1
protege47007 <davidak.a@yahoo.com>
2022-12-19 15:55:40 +0100

    directory chnage

---
+++ tailwind.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{jsx, js}",
+    "./public/index.html"
+  ],
+  theme: {
+    fontFamily: {
+      fontC: ['"Open Sans Condensed"', "sans-serif"]
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
