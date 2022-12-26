39532a8bd6c80226d521a80f1ab5a305c2109335
protege47007 <davidak.a@yahoo.com>
2022-12-26 22:06:41 +0100

    moved editor.js configuration to a separate file

--- src/components/dashboard/editor/config.jsx
+++ src/components/dashboard/editor/config.jsx
@@ -1,4 +1,29 @@
+import SimpleImage from "./SimpleImage"
 
-// const config = 
 
-// export default config
+const config = {
+    holder: "post",
+    logLevel: "VERBOSE",
+    placeholder: "Any new inspirations today?",
+    tools: {
+        image: SimpleImage
+    },
+    onChange: (api, event) => {
+      // saveProgress()
+      console.log('Now I know that Editor\'s content changed!', event)
+    },
+    data: {
+        time: 1552744582955,
+        blocks: [
+          {
+            type: "image",
+            data: {
+              url: "https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_1280.jpg"
+            }
+          }
+        ],
+        version: "2.11.10"
+    }
+  }
+
+export default config
